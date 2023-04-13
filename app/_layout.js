0687f00bab804c00988c831d9a578456dedaa76b
BilalThaha <bilal.athaha@gmail.com>
2023-04-13 22:32:42 +0530

    First commit

---
+++ app/_layout.js
@@ -0,0 +1,11 @@
+import { Stack } from "expo-router";
+
+const Layout = () => {
+    return <Stack/>
+}
+
+export default Layout;
+
+// export default function Layout() {
+//   return <Stack />;
+// }
---
+++ app/index.js
@@ -0,0 +1,24 @@
+import { View, Text } from "react-native";
+import { Link, Stack } from "expo-router";
+
+
+const Home = () => {
+    return(
+        <View>
+            <Text>Homer</Text>
+        </View>
+    )
+}
+
+export default Home;
+
+// export default function Home() {
+//   return (
+//     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+//       {/* Use the `Screen` component to configure the layout. */}
+//       <Stack.Screen options={{ title: "Overview" }} />
+//       {/* Use the `Link` component to enable optimized client-side routing. */}
+//       <Link href="/details">Go to Details</Link>
+//     </View>
+//   );
+// }
